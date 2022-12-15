67cb6cf1d558100ac6a16a8b86f70f2fffb403b7
Carsten Stahl <carstenstahl307@yahoo.com>
2022-12-15 07:53:40 +0100

    mediantest

--- R/mediantest.R
+++ R/mediantest.R
@@ -1,3 +1,18 @@
 mood_test <- function(data, alpha) {
+  ## checking validity #########################################################
+
+  ## normal routine ############################################################
+  # estimate the median
+  values <- data$value
+  N <- length(values)
+  order_stat <- sort(values)
+
+  if (N %% 2 == 0) {
+    median <- 0.5*(order_stat[N/2] + order_stat[N/2+1])
+  } else {
+    median <- oder_stat[(N+1)/2]
+  }
+
+  # compute m_1i
 
 }
